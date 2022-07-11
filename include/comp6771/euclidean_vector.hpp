37f2c3c823dbb2b74fecb1f716316d58eeab9724
z5260003 <z5260003@cse.unsw.edu.au>
2022-07-11 13:09:30 +1000

    Assignment 2 hpp file done

--- include/comp6771/euclidean_vector.hpp
+++ include/comp6771/euclidean_vector.hpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <list>
 
 namespace comp6771 {
 	class euclidean_vector_error : public std::runtime_error {
@@ -14,12 +16,80 @@ namespace comp6771 {
 
 	class euclidean_vector {
 	public:
-		// TODO
+		// Constructors
+		euclidean_vector();
+
+		explicit euclidean_vector(int);
+
+		euclidean_vector(int, double);
+
+		euclidean_vector(std::vector<double>::const_iterator, std::vector<double>::const_iterator);
+
+		euclidean_vector(std::initializer_list<double>);
+
+		euclidean_vector(euclidean_vector const&);
+
+		euclidean_vector(euclidean_vector&&);
+
+		//Destructor
+
+		auto operator=(euclidean_vector const&) -> euclidean_vector&;
+
+		auto operator=(euclidean_vector&&) noexcept -> euclidean_vector&;
+
+		auto operator[](int) const noexcept-> double;
+
+		auto operator[](int) noexcept -> double&;
+
+		auto operator+() const -> euclidean_vector;
+
+		auto operator-() const -> euclidean_vector;
+
+		auto operator+=(euclidean_vector const&) -> euclidean_vector&;
+
+		auto operator-=(euclidean_vector const&) -> euclidean_vector&;
+
+		auto operator*=(double) -> euclidean_vector&;
+
+		auto operator/=(double) -> euclidean_vector&;
+
+		explicit operator std::vector<double>() const;
+
+		explicit operator std::list<double>() const;
+
+		//Member functions
+		auto at(int) const -> double;
+
+		auto at(int) -> double&;
+
+		auto dimensions() const noexcept -> int;
+
+		friend auto operator==(euclidean_vector const&, euclidean_vector const&) -> bool;
+
+		friend auto operator!=(euclidean_vector const&, euclidean_vector const&) -> bool;
+
+		friend auto operator+(euclidean_vector const&, euclidean_vector const&) -> euclidean_vector;
+
+		friend auto operator-(euclidean_vector const&, euclidean_vector const&) -> euclidean_vector;
+
+		friend auto operator*(euclidean_vector const&, double) -> euclidean_vector;
+
+		friend auto operator*(double, euclidean_vector const&) -> euclidean_vector;
+
+		friend auto operator/(euclidean_vector const&, double) -> euclidean_vector;
+
+		friend auto operator<<(std::ostream&, euclidean_vector const&) -> std::ostream&;
 	private:
 		// ass2 spec requires we use std::unique_ptr<double[]>
 		// NOLINTNEXTLINE(modernize-avoid-c-arrays)
 		std::unique_ptr<double[]> magnitude_;
-		// TODO more if needed
+		int dimension_;
 	};
+	//Utiliyy functions
+	auto euclidean_norm(euclidean_vector const& v) -> double;
+
+	auto unit(euclidean_vector const& v) -> euclidean_vector;
+
+	auto dot(euclidean_vector const& x, euclidean_vector const& y) -> double;
 } // namespace comp6771
 #endif // COMP6771_EUCLIDEAN_VECTOR_HPP
